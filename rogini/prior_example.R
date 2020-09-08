d9e94fa8c16a0d3315df5274caf40c48d1c607bc
Loïc Chalmandrier <loic_chr@mailoo.org>
2020-09-08 16:39:03 +1200

    Create prior_example.R

---
+++ rogini/prior_example.R
@@ -0,0 +1,16 @@
+list_params <- c("mu", "sd")
+
+density <- function(pars){
+  names(pars)<- list_params
+  d1 <- dnorm(pars["mu"], -2, 0.1)
+  d2 <- dlnorm(pars["sd"], 2, 0.1)
+  return(d1+d2)
+}
+
+sampler <- function(n){
+  d1 <- rnorm(n, -2, 0.1)
+  d2 <- rlnorm(n, 2, 0.1)
+  cbind(d1, d2)
+}
+
+bounds <- data.frame(lower = c(-2.4,4), upper = c(-1.6,12))
