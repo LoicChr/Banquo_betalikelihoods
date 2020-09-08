62740ce320b7ee3cfa0fb5b45640f33cbe02858d
Loïc Chalmandrier <loic_chr@mailoo.org>
2020-09-08 16:41:32 +1200

    Update prior_example.R

--- rogini/prior_example.R
+++ rogini/prior_example.R
@@ -2,8 +2,8 @@ list_params <- c("mu", "sd")
 
 density <- function(pars){
   names(pars)<- list_params
-  d1 <- dnorm(pars["mu"], -2, 0.1)
-  d2 <- dlnorm(pars["sd"], 2, 0.1)
+  d1 <- dnorm(pars["mu"], -2, 0.1, log = T)
+  d2 <- dlnorm(pars["sd"], 2, 0.1, log = T)
   return(d1+d2)
 }
 
@@ -13,4 +13,4 @@ sampler <- function(n){
   cbind(d1, d2)
 }
 
-bounds <- data.frame(lower = c(-2.4,4), upper = c(-1.6,12))
+bounds <- data.frame(lower = c(-2.4,4), upper = c(-1.6,12), row.names = list_params)
